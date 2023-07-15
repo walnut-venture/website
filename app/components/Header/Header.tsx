216057c7ea643d7e82860112387c0cd3f58d6356
Grappler2010 <yakhyaev2008@mail.ru>
2023-07-15 19:10:17 +0300

    WIP

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import { Button, Container, Logo, Navigation } from "components";
+import { useWindowSize } from "hooks";
 
 import styles from "./header.module.scss";
 
 export const Header = () => {
+  // const { isLaptopS } = useWindowSize();
+
   return (
     <Container>
       <header className={styles.component}>
---
+++ app/hooks/useWindowSize.ts
@@ -0,0 +1,41 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+interface IWindowSize {
+  width: number;
+  height: number;
+};
+
+interface UseWindowSize extends IWindowSize {
+  isLaptopS: boolean;
+}
+
+export function useWindowSize(): UseWindowSize {
+  const [windowSize, setWindowSize] = useState<IWindowSize>({
+    width: 0,
+    height: 0
+  });
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const handleSize = () => {
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+        setWindowSize({
+          width,
+          height,
+        });
+      };
+      window.addEventListener("resize", handleSize);
+      handleSize();
+
+      return () =>window.removeEventListener("resize", handleSize);
+    }
+  }, []);
+
+  return {
+    ...windowSize,
+    isLaptopS: windowSize.width <= 700
+  };
+};
