6f2b5babfdaa5eb405e50ea9382cb77e69856549
Grappler2010 <yakhyaev2008@mail.ru>
2023-07-25 21:56:56 +0300

    WIP

--- app/components/ListItem/ListItem.tsx
+++ app/components/ListItem/ListItem.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import Image from "next/image";
 import { FunctionComponent } from "react";
 import { P } from "components";
@@ -7,11 +8,17 @@ import ellipseSrc from "./img/ellipse.svg";
 
 type TProps = {
   children: string;
+  className?: string;
 };
 
-export const ListItem:FunctionComponent<TProps> = ({ children }) => {
+export const ListItem:FunctionComponent<TProps> = ({
+  children,
+  className: propsClassName
+}) => {
+  const listClassName = classNames(styles.component, propsClassName);
+
   return (
-    <div className={styles.component}>
+    <div className={listClassName}>
       <Image className={styles.image} src={ellipseSrc} alt="Ellipse" />
       <P className={styles.title}>{children}</P>
     </div>
