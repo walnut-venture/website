603de4f8b6a59c86f25294a1c3d80f9d5d901c35
Grappler2010 <yakhyaev2008@mail.ru>
2023-07-31 21:43:02 +0300

    Change footer-content styles

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,30 +1,16 @@
 import { Contacts, Container, Logo, P } from "components";
-import { useWindowSize } from "hooks";
 
 import styles from "./footer.module.scss";
 
 export const Footer = () => {
-  const { isMobile } = useWindowSize();
-
   return (
     <Container>
       <footer className={styles.component}>
-        {
-          isMobile ?
-            <>
-              <div className={styles.contentWrapper}>
-                <Logo className={styles.footerLogo} />
-                <P className={styles.subtitle}>Copyright &copy; 2023 Walnut venture studios</P>
-              </div>
-              <Contacts />
-            </>
-            :
-            <>
-              <Logo className={styles.footerLogo} />
-              <Contacts />
-              <P className={styles.subtitle}>Copyright &copy; 2023 Walnut venture studios</P>
-            </>
-        }
+        <div className={styles.contentWrapper}>
+          <Logo className={styles.footerLogo} />
+          <P className={styles.subtitle}>Copyright &copy; 2023 Walnut venture studios</P>
+        </div>
+        <Contacts />
       </footer>
     </Container>
   );
