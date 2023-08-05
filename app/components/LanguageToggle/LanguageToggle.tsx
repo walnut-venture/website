fc67b6a613de8aa975a0d681a5dca6a25f2bee6e
dealwith <gleb.krishin@icloud.com>
2023-08-05 12:45:45 +0200

    Add active status for locale

--- app/components/LanguageToggle/LanguageToggle.tsx
+++ app/components/LanguageToggle/LanguageToggle.tsx
@@ -1,17 +1,25 @@
 import Link from "next-intl/link";
 import { P } from "components";
+import { useLocale } from 'next-intl';
 
 import styles from "./languageToggle.module.scss";
+import classNames from "classnames";
 
 export const LanguageToggle = () => {
+  const providedLocale = useLocale();
+
+  const toggleItemClassName = (locale: string) => classNames(styles.toggleItem, {
+    [styles.activeToggleItem]: providedLocale === locale
+  });
+
   return (
     <div className={styles.component}>
       <Link href="/" locale="en">
-        <P className={styles.toggleItem}>EN</P>
+        <P className={toggleItemClassName('en')}>EN</P>
       </Link>
       <P>/</P>
       <Link href="/" locale="de">
-        <P className={styles.toggleItem}>DE</P>
+        <P className={toggleItemClassName('de')}>DE</P>
       </Link>
     </div>
   );
