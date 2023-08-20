0d57f128f61f9a569bdba3eeaff170713f2719c6
Grappler2010 <yakhyaev2008@mail.ru>
2023-08-20 22:51:22 +0300

    Change MethodologySection component

--- app/components/MethodologySection/MethodologySection.tsx
+++ app/components/MethodologySection/MethodologySection.tsx
@@ -1,94 +1,133 @@
 import Image from "next/image";
-import { H2, H3, P, MainContainer } from "components";
+import { H2, H3, P, Container } from "components";
 import { useWindowSize } from "hooks";
 import { useTranslations } from "next-intl";
 
 import styles from "./methodologySection.module.scss";
-import arrowSrc from "./img/arrowIcon.svg";
 
 export const MethodologySection = () => {
-  const { isMobile } = useWindowSize();
+  const { isLaptopL } = useWindowSize();
   const t = useTranslations("Methodology");
 
   return (
-    <MainContainer>
+    <Container>
       <section id="methodology" className={styles.component}>
         <div className={styles.titleWrapper}>
           <H2>{t("mainTitle")}</H2>
           <H3 className={styles.subtitle}>{t("firstSubtitle")}</H3>
           <P>{t("firstText")}</P>
         </div>
-        <div className={styles.firstBlock}>
-          <div className={styles.container}>
-            <div className={styles.bgCard}>
-              <H3 className={styles.cardTitle}>{t("secondSubtitle")}</H3>
-              <P>{t("researchFirst")}</P>
-              <P>{t("researchSecond")}</P>
-            </div>
-            <div className={styles.card}>
-              <H3 className={styles.cardTitle}>{t("thirdSubtitle")}</H3>
-              <P>{t("studyFirst")}</P>
-              <P>{t("studySecond")}</P>
-              <P>{t("studyThird")}</P>
-            </div>
-          </div>
-        </div>
-        <div className={styles.secondBlock}>
-          <div className={styles.container}>
-            <div className={styles.card}>
-              <H3 className={styles.cardTitle}>{t("fourthSubtitle")}</H3>
-              <P>{t("preparationFirst")}</P>
-              <P>{t("preparationSecond")}</P>
-              <P>{t("preparationThird")}</P>
-              <P>{t("preparationFourth")}</P>
-            </div>
-            <div className={styles.bgCard}>
-              <H3 className={styles.cardTitle}>{t("fifthSubtile")}</H3>
-              <P>{t("scalingFirst")}</P>
-              <P>{t("scalingSecond")}</P>
-              <P>{t("scalingThird")}</P>
-              <P>{t("scalingFourth")}</P>
-            </div>
-          </div>
-        </div>
         {
-          isMobile ?
-            <div className={styles.phaseContainer}>
-              <div className={styles.phaseWrapper}>
-                <H3>{t("firstPhase")}</H3>
-                <Image className={styles.image} src={arrowSrc} alt="ArrowIcon" />
+          isLaptopL ?
+            <>
+              <div className={styles.cardsContainer}>
+                <div className={styles.firstCard}>
+                  <div className={styles.firstContent}>
+                    <H3 className={styles.cardTitle}>{t("secondSubtitle")}</H3>
+                    <P className={styles.cardText}>{t("researchFirst")}</P>
+                    <P className={styles.cardText}>{t("researchSecond")}</P>
+                  </div>
+                </div>
+                <div className={styles.secondCard}>
+                  <div className={styles.secondContent}>
+                    <H3 className={styles.cardTitle}>{t("thirdSubtitle")}</H3>
+                    <P className={styles.cardText}>{t("studyFirst")}</P>
+                    <P className={styles.cardText}>{t("studySecond")}</P>
+                    <P className={styles.cardText}>{t("studyThird")}</P>
+                  </div>
+                </div>
               </div>
-              <div className={styles.phaseWrapper}>
-                <H3>{t("secondPhase")}</H3>
-                <Image className={styles.image} src={arrowSrc} alt="ArrowIcon" />
+              <div className={styles.phaseContainer}>
+                <div className={styles.firstPhase}>
+                  <H3 className={styles.phaseTitle}>{t("firstPhase")}</H3>
+                </div>
+                <div className={styles.secondPhase}>
+                  <H3 className={styles.phaseTitle}>{t("secondPhase")}</H3>
+                </div>
               </div>
-              <div className={styles.phaseWrapper}>
-                <H3>{t("thirdPhase")}</H3>
-                <Image className={styles.image} src={arrowSrc} alt="ArrowIcon" />
+              <div className={styles.cardsContainer}>
+                <div className={styles.thirdCard}>
+                  <div className={styles.thirdContent}>
+                    <H3 className={styles.cardTitle}>{t("fourthSubtitle")}</H3>
+                    <P className={styles.cardText}>{t("preparationFirst")}</P>
+                    <P className={styles.cardText}>{t("preparationSecond")}</P>
+                    <P className={styles.cardText}>{t("preparationThird")}</P>
+                    <P className={styles.cardText}>{t("preparationFourth")}</P>
+                  </div>
+                </div>
+                <div className={styles.fourthCard}>
+                  <div className={styles.fourthContent}>
+                    <H3 className={styles.cardTitle}>{t("fifthSubtile")}</H3>
+                    <P className={styles.cardText}>{t("scalingFirst")}</P>
+                    <P className={styles.cardText}>{t("scalingSecond")}</P>
+                    <P className={styles.cardText}>{t("scalingThird")}</P>
+                    <P className={styles.cardText}>{t("scalingFourth")}</P>
+                  </div>
+                </div>
               </div>
-              <H3>{t("fourthPhase")}</H3>
-            </div>
+              <div className={styles.phaseContainer}>
+                <div className={styles.thirdPhase}>
+                  <H3 className={styles.phaseTitle}>{t("thirdPhase")}</H3>
+                </div>
+                <div className={styles.fourthPhase}>
+                  <H3 className={styles.phaseTitle}>{t("fourthPhase")}</H3>
+                </div>
+              </div>
+            </>
             :
-            <div className={styles.phaseContainer}>
-              <div className={styles.firstPhaseBlock}>
-                <div className={styles.phaseWrapper}>
-                  <H3>{t("firstPhase")}</H3>
-                  <Image className={styles.image} src={arrowSrc} alt="ArrowIcon" />
+            <>
+              <div className={styles.cardsContainer}>
+                <div className={styles.firstCard}>
+                  <div className={styles.firstContent}>
+                    <H3 className={styles.cardTitle}>{t("secondSubtitle")}</H3>
+                    <P className={styles.cardText}>{t("researchFirst")}</P>
+                    <P className={styles.cardText}>{t("researchSecond")}</P>
+                  </div>
                 </div>
-                <div className={styles.phaseWrapper}>
-                  <H3>{t("secondPhase")}</H3>
+                <div className={styles.secondCard}>
+                  <div className={styles.secondContent}>
+                    <H3 className={styles.cardTitle}>{t("thirdSubtitle")}</H3>
+                    <P className={styles.cardText}>{t("studyFirst")}</P>
+                    <P className={styles.cardText}>{t("studySecond")}</P>
+                    <P className={styles.cardText}>{t("studyThird")}</P>
+                  </div>
+                </div>
+                <div className={styles.thirdCard}>
+                  <div className={styles.thirdContent}>
+                    <H3 className={styles.cardTitle}>{t("fourthSubtitle")}</H3>
+                    <P className={styles.cardText}>{t("preparationFirst")}</P>
+                    <P className={styles.cardText}>{t("preparationSecond")}</P>
+                    <P className={styles.cardText}>{t("preparationThird")}</P>
+                    <P className={styles.cardText}>{t("preparationFourth")}</P>
+                  </div>
+                </div>
+                <div className={styles.fourthCard}>
+                  <div className={styles.fourthContent}>
+                    <H3 className={styles.cardTitle}>{t("fifthSubtile")}</H3>
+                    <P className={styles.cardText}>{t("scalingFirst")}</P>
+                    <P className={styles.cardText}>{t("scalingSecond")}</P>
+                    <P className={styles.cardText}>{t("scalingThird")}</P>
+                    <P className={styles.cardText}>{t("scalingFourth")}</P>
+                  </div>
                 </div>
               </div>
-              <div className={styles.lastPhaseBlock}>
-                <div className={styles.phaseWrapper}>
-                  <H3>{t("thirdPhase")}</H3>
-                  <Image className={styles.image} src={arrowSrc} alt="ArrowIcon" />
+              <div className={styles.phaseContainer}>
+                <div className={styles.firstPhase}>
+                  <H3 className={styles.phaseTitle}>{t("firstPhase")}</H3>
                 </div>
-                <H3>{t("fourthPhase")}</H3>
+                <div className={styles.secondPhase}>
+                  <H3 className={styles.phaseTitle}>{t("secondPhase")}</H3>
+                </div>
+                <div className={styles.thirdPhase}>
+                  <H3 className={styles.phaseTitle}>{t("thirdPhase")}</H3>
+                </div>
+                <div className={styles.fourthPhase}>
+                  <H3 className={styles.phaseTitle}>{t("fourthPhase")}</H3>
+                </div>
               </div>
-            </div>
+            </>
         }
       </section>
-    </MainContainer>
+    </Container>
   );
 };
